f5346d7e444146bf8e330d1e04b2dfe3c30f2c8b
cqiyi <cqiyi@hotmail.com>
2013-02-12 22:51:11 +0800

    实践课程考题整理.xls

--- 01_数据结构/exam001/test05/test05.cpp
+++ 01_数据结构/exam001/test05/test05.cpp
@@ -1,6 +1,13 @@
 //////////////////////////////////////////////////////
 //Ä£Ê½Æ¥Åä
 
+#define ListSize 100
+typedef int DataType;
+typedef struct{
+	DataType data[ListSize];
+	int length;
+}SeqList;
+
 #include <stdio.h>
 #include <string.h>
 
